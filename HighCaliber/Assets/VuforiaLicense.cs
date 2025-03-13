a41bb1424e92aefe55a17a7cad0b89a26e36da20
Nikola <nikolagoev9@gmail.com>
2025-03-13 11:26:56 +0100

    added correct sprite for new game button

---
+++ HighCaliber/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWZWTjA3UC8vLy8vQUFBQm1jMkV5Y1BxbkU4Q3J1akhXMHdOTTRnY29NYkJlLzNnOFpzY2lVczNySm1TM0lRaFk3UFF2YVlWWDgzZW54ZUlHQi91QUptM0FjSFlYcGRUYUFsdnNUclVJdlliVFNXSlpkQjZqU1pabVIzYjYralgydXFZdjBLY1UyRERrVkZJRUxTYk9iRDB0NmMwa2NSMUovMkVISy9HZWZ1UmEzY09RSStPK1V0RzcxdDhtcjdoQlBKNTJlcjJKSVJhZFpjQ1Z2aVJFdDV6SFRucjZ1SmdjTDRZcGZMakgzUGZzRHNBRWt1dWplR0pZcjFNaE1wZVJwdWpCNU5oOE1wTjY1MDhvaWxTQWNYTzJ0RGNjNXd3cjJ0eEgyTXdTdkFTVS9MTzJ2ejNia2k3ZEZZeEJvV0hBcmIzRGdCSmZKY29rOVNhMW11Q0N5eiszaytFWkNIdWlsdU10Zkd6WlhjRGNZVUhPSDk2dHVNcVlDRC8=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
